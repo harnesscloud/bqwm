9b74a405e71078fe3096586bd6ea8d71551587d7
Mark Stillwell <mark@stillwell.me>
2015-06-02 17:10:09 +0100

    okay, structure is coming along...

--- hello/api/service.py
+++ hello/api/service.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, json
 app = Flask(__name__)
 
+from ..schedulers import sched
+
 
 @app.route('/createReservation', methods=['POST'])
 def api_createReservation():
@@ -10,9 +12,9 @@ def api_createReservation():
             configs = request.json["Configurations"]
         except:
             pass
-    
+
         if len(configs) > 0:
-            return json.dumps(configs[0])
+            return json.dumps(sched(configs))
 
     return "no config"
 
---
+++ hello/schedulers/dummy.py
@@ -0,0 +1,5 @@
+def sched(configs):
+    if len(configs) > 0:
+        return configs[0]
+
+    return None
