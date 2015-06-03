bf0d5da6ca185b4d910938884ad703817f4b366a
Mark Stillwell <mark@stillwell.me>
2015-06-03 23:08:47 +0100

    okay, basics are in place...

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,9 +1,25 @@
+from flask import json
 import unittest
+from bqwm import api
 
-import sample
 
+class BqwmTestCase(unittest.TestCase):
 
-class TestSimple(unittest.TestCase):
-    
-    def test_failure(self):
-        self.assertTrue(False)
+    def setUp(self):
+        app = api.create_app()
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_true(self):
+        assert(True)
+
+    def test_checkReservation(self):
+        rv = self.app.get('/v2.0/checkReservation')
+        data = json.loads(rv.data)
+        assert(not data["Ready"])
+
+if __name__ == '__main__':
+    unittest.main()
