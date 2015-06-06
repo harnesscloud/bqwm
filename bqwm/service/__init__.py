b9ea989de94cd9e33123d052b5a0c44613ceb3d4
Mark Stillwell <mark@stillwell.me>
2015-06-07 00:14:32 +0100

    trying to do basic setup according to best practice conventions...

--- bqwm/service/__init__.py
+++ bqwm/service/__init__.py
@@ -1,24 +1,24 @@
-import os
-from flask import Flask
-
-from bqwm.service.views import api_v2_0
-
-
 class default_settings(object):
     SCHEDULER_STRATEGY = 'dummy'
 
 
 def create_app(cfg=None):
+    from flask import Flask
     app = Flask(__name__, instance_relative_config=True)
 
     load_config(app, cfg)
 
+    from bqwm.service.models import db
+    db.init_app(app)
+
+    from bqwm.service.views import api_v2_0
     app.register_blueprint(api_v2_0, url_prefix='/v2.0')
 
     return app
 
 
 def load_config(app, cfg):
+    import os
 
     app.config.from_object('bqwm.service.default_settings')
     app.config.from_pyfile('/etc/bqwm/default.cfg', silent=True)
