5722c784a13e035de31a8020192c7c450cbe436a
Mark Stillwell <mark@stillwell.me>
2015-06-08 17:21:07 +0100

    yeesh.

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get install curl python
-curl -s https://bootstrap.pypa.io/get-pip.py | python -
+/usr/bin/curl -s https://bootstrap.pypa.io/get-pip.py | python -
 pip install -r requirements.txt
 pip install -e .
 python setup.py test
