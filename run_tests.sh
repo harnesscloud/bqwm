20e19e40b651c37e50fa626d6b07f4958a49dec7
Mark Stillwell <mark@stillwell.me>
2015-06-08 17:32:39 +0100

    should work?

--- run_tests.sh
+++ run_tests.sh
@@ -3,5 +3,5 @@ apt-get update
 apt-get -y install curl python
 curl -s https://bootstrap.pypa.io/get-pip.py | python -
 pip install -r requirements.txt
-pip install -e .
+pip install coverage pytest
 python setup.py test
